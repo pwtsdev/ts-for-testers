c161c2d86ccbc7dd4bf5967206be5f3fb0d8273a
bkita <2436425+bkita@users.noreply.github.com>
2025-05-08 15:41:48 +0200

    M03L01 - variables

---
+++ src/course/M03/M03L01-variables.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+const firstName = 'Bartek';
+const age = 30;
+const isAdult = true;
+const person = {
+  name: 'Bartek Testowy',
+  age: 30,
+  isAdult: true,
+};
+
+console.log(firstName);
+console.log(age);
+console.log(isAdult);
+console.log(person.name);

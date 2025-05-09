14e5c2e28eeadebd0f6cf1c73446e4b8e3600f14
bkita <2436425+bkita@users.noreply.github.com>
2025-05-09 11:50:37 +0200

    M03L02 - var, let, const

---
+++ src/course/M03/M03L02-var-let-const.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+
+// let – a variable you can change
+let color = 'black';
+console.log(color);
+
+color = 'red';
+console.log(color);
+
+// const – a variable you cannot change
+const pesel = 123123123;
+console.log(pesel);
+
+// var - use let or const instead
+// var lastName = 'Testowy';
+// console.log(lastName);

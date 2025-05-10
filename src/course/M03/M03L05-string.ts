65300595dd4ed81fd45477b25ac8c00a09f2c12a
bkita <2436425+bkita@users.noreply.github.com>
2025-05-10 17:53:32 +0200

    M03L07 - string, part III

--- src/course/M03/M03L05-string.ts
+++ src/course/M03/M03L05-string.ts
@@ -51,3 +51,44 @@ console.log(titleFromWebsite);
 console.log(titleFromWebsite.trim());
 console.log(titleFromWebsite.trimStart());
 console.log(titleFromWebsite.trimEnd());
+
+// sprawdzenie zawartości
+const email = 'bartek@pwts.dev';
+console.log(email.includes('@'));
+console.log(email.startsWith('bar'));
+console.log(email.endsWith('.dev'));
+
+// wyszukiwanie
+const testText = 'Test and check, no room for bugs, smooth releases, happy hugs!';
+console.log(testText.search('bugs'));
+console.log(testText.search(/Bugs/i));
+console.log(testText.search(/ugs/i));
+
+// wycinanie części stringa
+// slice()
+// start: indeks początkowy (włącznie)
+// end: indeks końcowy (nie włącznie)
+console.log(testText.slice(0, 14));
+
+// ucinamy początek do końca
+console.log(testText.slice(5));
+
+// ucinamy od końca
+console.log(testText.slice(-5));
+
+// substring()
+// start: indeks początkowy (włącznie)
+// end: indeks końcowy (nie włącznie)
+console.log(testText.substring(0, 14));
+console.log(testText.substring(5));
+console.log(testText.substring(-5));
+
+// dzielenie stringów
+console.log(testText.split(' '));
+console.log(testText.split(','));
+
+// sprawdzenie indexu
+console.log(testText.charAt(0));
+
+// sprawdzenie indexu dla znaku
+console.log(email.indexOf('@'));

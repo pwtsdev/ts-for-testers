707daa914d6965926acf8c1544c6e91861a1d04e
bkita <2436425+bkita@users.noreply.github.com>
2025-05-10 17:31:47 +0200

    M03L01 - string, part II

--- src/course/M03/M03L05-string.ts
+++ src/course/M03/M03L05-string.ts
@@ -23,3 +23,31 @@ console.log('My name is ' + firstName + ' ' + lastName);
 
 // string literals / string templates
 console.log(`My name is ${firstName} ${lastName}`);
+
+// Zamiana stringów
+// wielkie/małe litery
+console.log(firstName.toUpperCase());
+console.log(lastName.toLocaleLowerCase());
+
+// zamiana znaków/słów
+const text = 'TypeScript is super boring!';
+console.log(text.replace('boring', 'COOL'));
+console.log(text.replaceAll(' ', '_'));
+
+// powtarzanie
+const title = 'a';
+console.log(title.repeat(129));
+
+// długość stringa
+console.log(title.length);
+
+// dopełnienie
+console.log(firstName.padStart(firstName.length + 4, 'x'));
+console.log(firstName.padEnd(firstName.length + 4, 'x'));
+
+// usuwanie białych znaków
+const titleFromWebsite = '  TypeScript is awesome!  ';
+console.log(titleFromWebsite);
+console.log(titleFromWebsite.trim());
+console.log(titleFromWebsite.trimStart());
+console.log(titleFromWebsite.trimEnd());

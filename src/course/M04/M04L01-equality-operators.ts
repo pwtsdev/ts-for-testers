cc969de2249087d317dd31efe53d626be8369b2e
bkita <2436425+bkita@users.noreply.github.com>
2025-05-12 11:03:03 +0200

    M04L01 - equality operators

---
+++ src/course/M04/M04L01-equality-operators.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
+/* eslint-disable no-console */
+export {};
+
+const a = null;
+const b = undefined;
+
+console.log(typeof a);
+console.log(typeof b);
+
+//  My advice is to never use the evil twins!!!
+console.log(a === b);

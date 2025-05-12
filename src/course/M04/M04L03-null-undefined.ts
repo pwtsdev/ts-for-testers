ffc26957ad563adbb197024972e4a6be7066eea4
bkita <2436425+bkita@users.noreply.github.com>
2025-05-12 13:52:28 +0200

    M04L03 - null vs. undefined

---
+++ src/course/M04/M04L03-null-undefined.ts
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+export {};
+
+// let session = 'q3easdfawerfsdfve4ffdv';
+// let cache: string;
+
+// console.log(`Cache: ${cache}`);
+
+// console.log(`Sesja jest ustawiona: ${session}`);
+
+// // ...... //
+// session = null;
+// console.log(`Sesja jest usunięta: ${session}`);
+
+// // Typy null i undefined
+// console.log(`type null: ${typeof session}`);
+// console.log(`type undefined: ${typeof cache}`);

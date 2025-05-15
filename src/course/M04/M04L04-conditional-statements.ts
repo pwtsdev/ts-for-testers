1d133313533022b4a20fbfdfc2c6effc63e042f1
bkita <2436425+bkita@users.noreply.github.com>
2025-05-15 14:13:44 +0200

    M04L04 - conditional statements part I

---
+++ src/course/M04/M04L04-conditional-statements.ts
@@ -0,0 +1,30 @@
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-unnecessary-condition */
+/* eslint-disable no-console */
+export {};
+
+let isRaining = false;
+
+if (isRaining) {
+  console.log('I need to take an umbrella!');
+} else {
+  console.log('I need to take sun glasses!');
+}
+
+// if (prawda) {
+//     wykonaj kod jeśli prawda
+// }
+
+// if (prawda) {
+//     wykonaj kod jeśli prawda
+// } else {
+//     wykonaj kod jeśli fałsz
+// }
+
+// if (prawda) {
+//     wykonaj kod jeśli prawda
+// } else if (inny warukek prawda) {
+//     wykonaj kod jeśli inny warunek
+// } else {
+//     wykonaj kod w pozostałych przypadkach
+// }

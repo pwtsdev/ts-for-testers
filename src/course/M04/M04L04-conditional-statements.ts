5d6cea36f11d5f31138b4694a49c1a8f59bb92df
bkita <2436425+bkita@users.noreply.github.com>
2025-05-15 14:27:15 +0200

    M04L05 - conditional statements part II

--- src/course/M04/M04L04-conditional-statements.ts
+++ src/course/M04/M04L04-conditional-statements.ts
@@ -3,12 +3,32 @@
 /* eslint-disable no-console */
 export {};
 
-let isRaining = false;
+let isRaining = true;
 
 if (isRaining) {
   console.log('I need to take an umbrella!');
 } else {
   console.log('I need to take sun glasses!');
+}
+
+// warunek ternarny ? : - skrócony zapis
+// jako zamiennik if / else
+// console.log(warunek ? prawda : fałsz);
+
+const weatherForecast = isRaining ? 'I need to take an umbrella!' : 'I need to take sun glasses!';
+console.log(weatherForecast);
+
+const ENV = 'PROD';
+console.log(ENV === 'PROD' ? 'Odpalam testy na prodzie!' : 'Odpalam testy na QA!');
+
+let statusCode = '500';
+
+if (statusCode.startsWith('2')) {
+  console.log('Status code: 2xx');
+} else if (statusCode.startsWith('4')) {
+  console.log('Status code 4xx');
+} else {
+  console.log('Unknown status code');
 }
 
 // if (prawda) {

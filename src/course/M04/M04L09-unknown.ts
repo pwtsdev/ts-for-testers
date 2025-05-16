a9dba14c31dc62774b600b4d50da050c6647740d
bkita <2436425+bkita@users.noreply.github.com>
2025-05-16 11:17:50 +0200

    M04L09 - type unknown

---
+++ src/course/M04/M04L09-unknown.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-console */
+export {};
+
+let name: unknown;
+name = 'bartek';
+
+if (typeof name === 'string') {
+  console.log(name.toUpperCase());
+}

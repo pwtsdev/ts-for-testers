8db702866b564f03222eebd89272d9ac2e37e905
bkita <2436425+bkita@users.noreply.github.com>
2025-05-16 11:06:24 +0200

    M04L08 - type any

---
+++ src/course/M04/M04L08-any.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
+export {};
+
+// "Use any when you must, not when you can!!!"
+
+let name: any;
+name = 'null';
+console.log(name.toLocaleUpperCase());

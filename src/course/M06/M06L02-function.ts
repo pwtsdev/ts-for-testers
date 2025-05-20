e59ab9575aebd3b3a199c40320ceb0680f2513aa
bkita <2436425+bkita@users.noreply.github.com>
2025-05-20 14:15:29 +0200

    M06L02 - first function

---
+++ src/course/M06/M06L02-function.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+export {};
+
+function makeASandwich(): void {
+  console.log('Prepare a slice of bread');
+  console.log('Spread butter on the bread');
+  console.log('Prepare the cheese');
+  console.log('Add a slice of tomato');
+  console.log('Sprinkle with salt');
+  console.log('Sandwich is ready!!!');
+}
+
+makeASandwich();
+makeASandwich();
+makeASandwich();
+makeASandwich();

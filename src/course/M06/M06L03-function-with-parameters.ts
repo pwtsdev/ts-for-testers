a727a26576b13d59ddd697954fb2b29b5c8af640
bkita <2436425+bkita@users.noreply.github.com>
2025-05-20 14:54:36 +0200

    M06L03 - function with parameters

---
+++ src/course/M06/M06L03-function-with-parameters.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+export {};
+
+// Parametry – podczas definicji funkcji:
+function addTwoNumbers(a: number, b: number): void {
+  console.log(a + b);
+}
+
+// Argumenty – podczas wywołania funkcji:
+addTwoNumbers(100, 20);
+addTwoNumbers(-10, 10);
+addTwoNumbers(500, 500);

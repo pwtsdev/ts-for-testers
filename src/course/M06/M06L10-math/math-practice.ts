6fd3ad10a7d5517b1f8c4c4a69722c30f369aa95
bkita <2436425+bkita@users.noreply.github.com>
2025-05-23 10:31:27 +0200

    M06L10 - import export

---
+++ src/course/M06/M06L10-math/math-practice.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+
+import { add, divide, multiply, PI, subtract } from './math';
+
+console.log(add(10, 20));
+console.log(subtract(20, 10));
+console.log(multiply(10, 10));
+console.log(divide(20, 2));
+console.log(PI);
---
+++ src/course/M06/M06L10-math/math.ts
@@ -0,0 +1,20 @@
+export const PI = 3.14;
+
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+export function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error('Division by zero is not allowed');
+  }
+  return a / b;
+}

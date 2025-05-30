c4caddb395a0d7860c38a79a28d9b3d7b98aceb4
bkita <2436425+bkita@users.noreply.github.com>
2025-05-30 11:06:01 +0200

    M07L01 - arrays - part I

---
+++ src/course/M07/M07L01-arrays-part-I.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+console.log('Tworzenie pustej  listy:');
+const names = new Array<string>(10);
+console.log(names);
+
+const emptyArray: string[] = [];
+console.log(emptyArray);
+
+console.log('Tworzenie listy z elementami:');
+const boys: string[] = ['Janek', 'Tomek', 'Bartek', 'Krzysiek'];
+console.log(boys);
+
+const numbers: number[] = [20, 10, 30, 5];
+console.log(numbers);
